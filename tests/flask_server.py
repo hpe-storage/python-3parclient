7bb1d1537ff4d5acd3c147acd5ae3e480880d613
Walter A. Boring IV <walter.boring@hp.com>
2012-09-10 15:07:52 -0700

    moved

--- tests/flask_server.py
+++ tests/flask_server.py
@@ -1,13 +1,23 @@
+import pprint
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
     return 'Hello World'
 
-@app.route('/api/v1/credentials')
+@app.route('/api/v1/credentials', methods=['GET', 'POST'])
 def credentials():
-    return 'credentials called'
+    if request.method == 'POST':
+        pprint.pprint("Request = %s" % request)
+        pprint.pprint("data = %s" % request.data)
+        pprint.pprint("Headers = %s" % request.headers)
+        return 'POST credentials called'
+    else:
+        return 'GET credentials called'
+
 
 if __name__ == "__main__":
     app.run()
