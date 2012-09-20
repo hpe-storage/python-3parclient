e40bfe96a5bd9b9ee0b20841a3f20cd23078d433
Walter A. Boring IV <walter.boring@hp.com>
2012-09-20 14:19:34 -0700

    more cleanup

--- tests/3par_cpg_test.py
+++ tests/3par_cpg_test.py
@@ -24,113 +24,6 @@ cl = client.HP3ParClient("http://10.10.22.241:8008/api/v1")
 if "debug" in args and args.debug == True:
     cl.debug_rest(True)
 
-
-def test_logout():
-    print "Test Logout"
-    try: 
-       cl.login(username, password)
-       pprint.pprint("Login worked")
-    except exceptions.Unauthorized as ex:
-       pprint.pprint("Login Failed")
-
-    try: 
-       cl.logout()
-       pprint.pprint("Logout worked")
-    except exceptions.Unauthorized as ex:
-       pprint.pprint("Logout Failed")
-
-def get_volumes():
-    print "Get Volumes"
-    try:
-       volumes = cl.getVolumes()
-       pprint.pprint(volumes)
-    except exceptions.Unauthorized as ex:
-       pprint.pprint("You must login first")
-    except Exception as ex:
-       print ex
-    print "Complete\n"
-
-
-def test_create_volume():
-    print "Create Volumes"
-    try:
-       cl.login(username, password)
-       cl.createVolume("Volume1", "someCPG", "300")
-       cl.createVolume("Volume2", "anotherCPG", 1024, 
-                                {'comment': 'something', 'snapCPG':'somesnapcpg'})
-
-    except exceptions.Unauthorized as ex:
-       pprint.pprint("You must login first")
-    except Exception as ex:
-       print ex
-
-    try:
-       cl.createVolume("Volume3", "testCPG", 2048, "foo")
-    except Exception as ex:
-       pass
-
-    try:
-	volume = cl.createVolume("VolumeBad", "testCPG", 2048, {'bogus':'break'})
-    except exceptions.BadRequest as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	volume = cl.createVolume("VolumeExists", "testCPG", 2048)
-    except exceptions.Conflict as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	volume = cl.createVolume("VolumeTooLarge", "testCPG", 10241024)
-    except exceptions.BadRequest as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	volume = cl.createVolume("VolumeNotEnoughSpace", "testCPG", 9999)
-    except exceptions.BadRequest as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    print "Complete\n"
-
-def test_delete_volume():
-    print "Test Delete Volume"
-
-    try:
-	cl.deleteVolume("foo")
-    except exceptions.NotFound as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	cl.deleteVolume("forbidden")
-    except exceptions.Forbidden as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	cl.deleteVolume("retained")
-    except exceptions.Forbidden as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	cl.deleteVolume("readonlychild")
-    except exceptions.Forbidden as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    try:
-	cl.deleteVolume("works")
-    except exceptions.NotFound as ex:
-	print "Got Expected Exception %s" % ex
-        pass
-
-    print "Complete\n"
-
-
 def get_CPGs():
     print "Get CPGs"
     try:
@@ -139,15 +32,6 @@ def get_CPGs():
     except Exception as ex:
        print ex
     print "Complete\n"
-
-def get_VLUNs():
-    print "Get VLUNs"
-    try:
-       vluns = cl.getVLUNs()
-       pprint.pprint(vluns)
-    except Exception as ex:
-       print ex
-
 
 def create_CPG():
     print "Create CPGs"
