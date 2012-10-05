9e6f14572ca2422f7ba0fa43c484060ffc0ec004
Walter A. Boring IV <walter.boring@hp.com>
2012-10-05 15:48:10 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+import hp3parclient
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+import sys
+
+
+setup(
+  name='hp3parclient',
+  version=hp3parclient.version,
+  description="HP 3PAR HTTP REST Client",
+  author="Walter A. Boring IV",
+  author_email="walter.boring@hp.com",
+  maintainer="Walter A. Boring IV",
+  keywords=["hp", "3par", "rest"],
+  requires=['httplib2(>=0.6.0)'],
+  install_requires=['httplib2 >= 0.6.0'],
+  tests_require=["nose"],
+  license="Apache License, Version 2.0",
+  packages=find_packages(),
+  provides=['hp3parclient'],
+  classifiers=[
+     'Development Status :: 3 - Alpha',
+     'Intended Audience :: Developers',
+     'License :: OSI Approved :: Apache Software License',
+     'Environment :: Web Environment',
+     'Programming Language :: Python',
+     'Programming Language :: Python :: 2.6',
+     'Programming Language :: Python :: 2.7',
+     'Topic :: Internet :: WWW/HTTP',
+     
+     ]
+  )
