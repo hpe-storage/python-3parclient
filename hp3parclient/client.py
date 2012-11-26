30a5eb036912bbb7b1cecc512c5a568ba066268a
Walter A. Boring IV <walter.boring@hp.com>
2012-11-26 10:11:47 -0800

    make the deleteVLUN api consistent with other VLUN methods

--- hp3parclient/client.py
+++ hp3parclient/client.py
@@ -536,11 +536,11 @@ class HP3ParClient:
         else:
             return None
         
-    def deleteVLUN(self, name, lunID, hostname=None, port=None):
+    def deleteVLUN(self, volumeName, lunID, hostname=None, port=None):
         """ 
         Delete a VLUN
         
-        :param name: the volume name of the VLUN
+        :param volumeName: the volume name of the VLUN
         :type name: str
         :param lunID: The LUN ID 
         :type lunID: int
@@ -560,7 +560,7 @@ class HP3ParClient:
         :raises: :class:`~hp3parclient.exceptions.HTTPForbidden` - PERM_DENIED - Permission denied
         """
 
-        vlun = "%s,%s" % (name, lunID)
+        vlun = "%s,%s" % (volumeName, lunID)
 
         if hostname:
             vlun += ",%s" % hostname
