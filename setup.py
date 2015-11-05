25611b7900ea792e8245036c8869a30c9f1015e6
Anthony Lee <anthony.mic.lee@hpe.com>
2015-11-04 17:15:35 -0800

    Updating Documentation
    The client documentation is being updated to use the new
    PyPi project name, 'python-3parclient'.
    
    Change-Id: I813a34ade8cfb989952ba5d98fc81bebeb2ab381

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 
 setup(
-  name='hpe3parclient',
+  name='python-3parclient',
   version=hpe3parclient.version,
   description="HPE 3PAR HTTP REST Client",
   author="Walter A. Boring IV",
@@ -20,7 +20,7 @@ setup(
   license="Apache License, Version 2.0",
   packages=find_packages(),
   provides=['hpe3parclient'],
-  url="http://packages.python.org/hpe3parclient",
+  url="http://packages.python.org/python-3parclient",
   classifiers=[
      'Development Status :: 5 - Production/Stable',
      'Intended Audience :: Developers',
